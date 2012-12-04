6a37c247a5f608ee62c899887261362b2ccfa1f9
Adrian Gose <agose35226@yahoo.com>
2012-12-03 19:52:24 -0600

    PlotDeflect final function awaiting
    Also changed gitignore to attempt to ignore .asv files.

--- FinalTestProgram/plotdeflect.m
+++ FinalTestProgram/plotdeflect.m
@@ -1,12 +1,4 @@
 function [plot1,plot2] = plotdeflect(x1 ,x2, y1, y2)
 
-
-hold off
-plot1 = plot(-x1,-y1);       
-hold on
-plot2 = plot(-x2,-y2);        
-hold off
-title('Deflection of Beam')
-ylabel('Y Axis')
-xlabel('X Axis')
+%% Removed For Testing: Awaiting final Plot Function
 end
