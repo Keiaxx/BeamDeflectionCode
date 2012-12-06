ff2e2302d96b0fb17bfe3e600138706146d6e3bf
Adrian Gose <agose35226@yahoo.com>
2012-12-05 22:13:20 -0600

    Added progress percentage

--- FinalTestProgram/beam_deflection_final_3.m
+++ FinalTestProgram/beam_deflection_final_3.m
@@ -313,8 +313,8 @@ it = 1;
 barnum = 1;
 wbgui = waitbar(0, 'Calculating...');
 while barnum <= 100
-    waitbar(barnum/100)
-    randpause = 0.005 + (0.04-0.005).*rand(100,1);
+     waitbar(barnum/100,wbgui,['Calculating.                                 Progress:  ' num2str(barnum/100*100) '%'])
+    randpause = 0.005 + (0.08-0.005).*rand(100,1);
     pause(randpause)
     barnum = barnum + 1;
 end
